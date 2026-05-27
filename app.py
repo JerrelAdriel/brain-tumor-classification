9e8c03ade6a56f2bf098834743f3e36f0a2e2c72
JerrelAdriel <adriel.jerrel@gmail.com>
2026-05-27 21:36:03 +0700

    fix: security and performance improvements
    - Move app.run to if __name__ == '__main__' guard
    - Disable debug mode in production
    - Load ML model once at startup instead of per-request
    - Clean up uploaded files after prediction
    - Add input validation for empty file submission
    - Add requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask, render_template, request
 from helper import Helper
 from werkzeug.utils import secure_filename
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = {'.jpg'}
+
+MODEL = load_model("models/vgg16_model_scen2confv1.h5")
 
 @app.route('/', methods = ['GET'])
 def index():
@@ -16,19 +20,27 @@ def index():
 
 @app.route('/predict', methods=['POST','GET'])
 def upload_files():
-    prediction = None 
+    prediction = None
     if request.method == 'POST':
         uploaded_file = request.files['imageFile']
+        if uploaded_file.filename == '':
+            return render_template('index.html', prediction='No file selected')
         filename = secure_filename(uploaded_file.filename)
         if filename != '':
             message = Helper().check_file(filename,ALLOWED_EXTENSIONS)
             if message:
                 return render_template('predict.html', message=message)
             else:
-                uploaded_file.save("static/img_uploaded/"+filename)
-        
-        prediction = Helper().tumor_predict(filename)    
+                filepath = "static/img_uploaded/" + filename
+                uploaded_file.save(filepath)
+
+        prediction = Helper().tumor_predict(filename, MODEL)
+        try:
+            os.remove(filepath)
+        except:
+            pass
         return render_template('predict.html', prediction = prediction, filename=filename)
     return render_template('predict.html', prediction = prediction)
 
-app.run(host="localhost", port=8000, debug=True)
+if __name__ == "__main__":
+    app.run(host="localhost", port=8000, debug=False)
--- helper.py
+++ helper.py
@@ -11,17 +11,16 @@ class Helper(object):
         if file_ext.lower() not in ext:
             message = "Cannot Predict File "+file_ext+", Format File Not Allowed! Please Upload File .jpg"
             return message
-        
-    def tumor_predict(self, brain_image):
+
+    def tumor_predict(self, brain_image, model):
         # classes = []
         treshold = 0.5
         message_tumor = "Brain Has Tumor"
         message_no_tumor = "Brain Has No Tumor"
-        model = load_model("models/vgg16_model_scen2confv1.h5")
         image = load_img("static/img_uploaded/"+brain_image, target_size = (224,224))
         x = img_to_array(image)
         x = numpy.expand_dims(x, axis = 0)
-        
+
         # start_time = time.time()
         classification = model.predict(x)
         # pred_time = time.time() - start_time
@@ -35,11 +34,11 @@ class Helper(object):
             # classes = [1]
             # classification = numpy.round(classification)
             result = message_tumor
-        
+
         # acc,pre,sen,f1score = self.perform_model(classes, classification)
         return result
     # , acc, pre, sen, f1score, pred_time
-    
+
     # def perform_model(self, classes, classification):
     #     conf_mat = confusion_matrix(classes, classification, labels = [0,1])
     #     true_positive = conf_mat[0][0]
@@ -60,6 +59,5 @@ class Helper(object):
     #         sen = round(true_negative / (true_negative + false_positive) * 100)
     #         # spe = round(true_positive / (true_positive + false_negative) * 100, 1)
     #         f1score = round(2 * (pre * sen) / (pre + sen))
-            
+
     #     return acc, pre, sen, f1score
-    
